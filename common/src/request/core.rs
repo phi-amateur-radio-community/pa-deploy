deb0e1772e85c180997333babfaa720f8f94355a
rangeset <2432647223@qq.com>
2026-05-12 22:35:55 +0800

    feat(http): add http request builder and string formatter

--- common/src/request/core.rs
+++ common/src/request/core.rs
@@ -13,7 +13,57 @@ use std::collections::HashMap;
 #[allow(unused)]
 pub struct HttpRequest {
     method: HttpMethod,
+    uri: String,
     version: HttpVersion,
     header: HashMap<String, String>,
     body: String,
+}
+
+#[allow(unused)]
+impl HttpRequest {
+    pub fn new(method: HttpMethod, uri: String, version: HttpVersion) -> Self {
+        HttpRequest {
+            method,
+            uri,
+            version,
+            header: HashMap::new(),
+            body: String::from(""),
+        }
+    }
+
+    pub fn add_header(&mut self, key: String, value: String) {
+        self.header.insert(key, value);
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut s = String::from("");
+        s.push_str(self.method.to_string());
+        s.push(' ');
+        s.push_str(&self.uri);
+        s.push(' ');
+        s.push_str(self.version.to_string());
+        add_crlf(&mut s);
+
+        let mut keys: Vec<&str> = self.header.keys().map(|s| s.as_str()).collect();
+        keys.sort();
+        for key in keys {
+            self.push_header(&mut s, key);
+        }
+        add_crlf(&mut s);
+
+        s.push_str(&self.body);
+        s
+    }
+
+    fn push_header(&self, s: &mut String, key: &str) {
+        let value = &self.header[key];
+        s.push_str(key);
+        s.push_str(": ");
+        s.push_str(value);
+        add_crlf(s);
+    }
+}
+
+fn add_crlf(s: &mut String) {
+    s.push_str("\r\n")
 }
