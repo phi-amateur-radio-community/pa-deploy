9080af8d228397797c41aa2b9815d6eb175a2bf5
rangeset <2432647223@qq.com>
2026-05-18 20:00:28 +0800

    feat(ui): add header and footer

--- server/src/ui.rs
+++ server/src/ui.rs
@@ -7,18 +7,22 @@
 // TUI
 
 use crate::conf::Config;
-use ratatui::crossterm::{
-    event::{self, Event, KeyCode},
-    execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
-};
-use std::io;
-
 use ratatui::{
-    Terminal,
+    Frame, Terminal,
     backend::CrosstermBackend,
+    crossterm::{
+        event::{self, Event, KeyCode},
+        execute,
+        terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+    },
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    text::Line,
     widgets::{Block, Borders, Paragraph},
 };
+use std::io;
+use std::rc::Rc;
+
+const FOOTER_CONTENT: [&str; 3] = ["Create [C]", "Quit [Q]", "Remove [R]"];
 
 #[derive(Debug, thiserror::Error)]
 pub enum UiError {
@@ -36,12 +40,24 @@ pub fn config(_config: &mut Config) -> Result<(), UiError> {
     loop {
         terminal.draw(|f| {
             let area = f.area();
-            let block = Block::default()
-                .title("PA Deploy Client Configuration Manager")
-                .borders(Borders::ALL);
 
-            let text = Paragraph::new("Hello World").block(block);
-            f.render_widget(text, area);
+            let screen = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Min(0), Constraint::Length(3)])
+                .split(area);
+
+            let title = format!(
+                "PA Deploy Client Configuration Manager Version {}",
+                env!("CARGO_PKG_VERSION")
+            );
+            let title_block = Block::new()
+                .borders(Borders::ALL)
+                .title(Line::raw(title).alignment(Alignment::Center));
+            f.render_widget(&title_block, screen[0]);
+
+            let _ = render_footer(f, &FOOTER_CONTENT, screen[1]);
+
+            let _inner = title_block.inner(screen[0]);
         })?;
 
         if event::poll(std::time::Duration::from_millis(50))?
@@ -56,4 +72,18 @@ pub fn config(_config: &mut Config) -> Result<(), UiError> {
     execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
 
     Ok(())
+}
+
+fn render_footer(f: &mut Frame, contents: &[&str], area: Rect) -> Rc<[Rect]> {
+    let count = contents.len();
+    let constraints = vec![Constraint::Fill(1); count];
+    let footer = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(constraints)
+        .split(area);
+    for (msg, location) in contents.iter().zip(footer.iter()) {
+        let text = Paragraph::new(*msg).alignment(Alignment::Center);
+        f.render_widget(&text, *location);
+    }
+    footer
 }
